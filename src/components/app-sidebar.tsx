74279a4d838b7027c6d8aaf8a731e325e682b83f
adivikramp <adileads1@gmail.com>
2025-05-23 01:12:53 +0530

    style: Add Sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -11,7 +11,7 @@ import { Badge } from "./ui/badge"
 
 export default function AppSidebar() {
     return (
-        <Sidebar className="border-none w-[800px]">
+        <Sidebar className="border-none w-[800px]" style={{ '--sidebar-width': '800px' } as React.CSSProperties}>
             <SidebarContent className="flex justify-between items-center h-full bg-black">
                 <SidebarHeader className="flex flex-col justify-center text-white gap-y-4 mt-16">
                     <Badge variant="outline" className="flex border-2 text-white border-white rounded-full px-6 py-2 gap-x-3">
